57352c4de1ce4abdeddc6698080d6f90cb8425d3
sun <1400017665@pku.edu.cn>
2018-09-06 13:56:20 +0000

    Add passowrd resets

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -32,7 +32,7 @@ class SessionsController extends Controller
                 return redirect()->intended(route('users.show',[Auth::user()]));
             }else{
                 Auth::logout();
-                session()->flash('warning','您的账号未激活，请检查邮箱中的注册邮件进行激活。')
+                session()->flash('warning','您的账号未激活，请检查邮箱中的注册邮件进行激活。');
                 return redirect('/');
             }
 
